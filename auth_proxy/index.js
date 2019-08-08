ada11bc55885c8f092fa2e80dcb5db9c32189c31
kg0r0 <kg0r0@yahoo.co.jp>
2019-08-08 21:36:37 +0900

    add basic auth

--- auth_proxy/index.js
+++ auth_proxy/index.js
@@ -1,20 +1,30 @@
 const express = require('express');
+const auth = require('basic-auth');
 const Redis = require('ioredis');
 const app = express();
+const compare = require('tsscmp');
 
 app.get('/', async (req, res) => {
   const redis = new Redis({
     host: 'redis',
   });
   const pong = await redis.ping();
+  const credentials = auth(req);
   console.log(pong);
   redis.disconnect();
-  if (!req.headers['authorization']) {
-    res.status(401).send("NG");
+  if (!credentials || !check(credentials.name, credentials.pass)) {
+    res.status(401).send('Access denied');
   } else {
-    res.status(200).send("OK");
+    res.status(200).send('Access granted');
   }
 });
+
+function check(name, pass) {
+  let valid = true;
+  valid = compare(name, 'name') && valid
+  valid = compare(pass, 'password') && valid
+  return valid
+}
 
 app.listen(3000, () => {
   console.log('Server running at http://127.0.0.1:3000')
